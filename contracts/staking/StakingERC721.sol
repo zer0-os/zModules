31f436b19b7672b6dc1922db82a27d2efeb51ab8
James Earle <j_earle@hotmail.com>
2024-04-17 13:20:42 -0700

    formatting

--- contracts/staking/StakingERC721.sol
+++ contracts/staking/StakingERC721.sol
@@ -9,8 +9,8 @@ import { IStakingERC721 } from "./IStakingERC721.sol";
 
 /**
  * @title Staking721
- * @notice A staking contract that allows depositing ERC721 tokens and mints a 
- * non-transferable ERC721 token in return as representation of the deposit. 
+ * @notice A staking contract that allows depositing ERC721 tokens and mints a
+ * non-transferable ERC721 token in return as representation of the deposit.
  */
 contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
     /**
@@ -39,8 +39,8 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
     uint256 public timeLockPeriod;
 
     /**
-	 * @dev Track for each stake when it was most recently accessed
-	 */
+     * @dev Track for each stake when it was most recently accessed
+     */
     mapping(address staker => Staker stakerData) public stakers;
 
     modifier onlySNFTOwner(uint256 tokenId) {
@@ -51,14 +51,14 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
     }
 
     constructor(
-		string memory name,
-		string memory symbol,
+        string memory name,
+        string memory symbol,
         IERC721 _stakingToken,
         IERC20 _rewardsToken,
         uint256 _poolWeight,
         uint256 _periodLength,
         uint256 _timeLockPeriod
-	) ERC721NonTransferrable(name, symbol) {
+    ) ERC721NonTransferrable(name, symbol) {
         // _createPool(_config);
         stakingToken = _stakingToken;
         rewardsToken = _rewardsToken;
@@ -66,17 +66,17 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
         periodLength = _periodLength;
         timeLockPeriod = _timeLockPeriod;
         // config = _config;
-	}
+    }
 
-	/**
-	 * @notice Stake one or more ERC721 tokens and receive non-transferable ERC721 tokens in return
-	 * @param tokenIds Array of tokenIds to be staked by the caller
-	 */
+    /**
+     * @notice Stake one or more ERC721 tokens and receive non-transferable ERC721 tokens in return
+     * @param tokenIds Array of tokenIds to be staked by the caller
+     */
     function stake(uint256[] calldata tokenIds) external override {
         Staker storage staker = stakers[msg.sender];
 
         if (staker.numStaked > 0) {
-	        // It isn't their first stake, snapshot pending rewards
+            // It isn't their first stake, snapshot pending rewards
             staker.pendingRewards = _getPendingRewards(staker);
         } else {
             // Log the time at which this stake becomes claimable or unstakable
@@ -86,7 +86,7 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
 
         uint256 i;
         uint256 len = tokenIds.length;
-        for (i ; i < len;) {
+        for (i; i < len; ) {
             _stake(tokenIds[i]);
 
             unchecked {
@@ -94,14 +94,14 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
             }
         }
 
-		staker.numStaked += len;
+        staker.numStaked += len;
         staker.lastUpdatedTimestamp = block.timestamp;
     }
 
-	/**
-	 * @notice Claim rewards for all staked ERC721 tokens
+    /**
+     * @notice Claim rewards for all staked ERC721 tokens
      * @dev Will revert if the time lock period has not been met
-	 */
+     */
     function claim() external override {
         Staker storage staker = stakers[msg.sender];
         _claim(staker);
@@ -121,7 +121,7 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
 
         uint256 i;
         uint256 len = tokenIds.length;
-        for (i; i < len;) {
+        for (i; i < len; ) {
             uint256 tokenId = tokenIds[i];
             _unstake(tokenId, staker);
 
@@ -139,21 +139,26 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
     /**
      * @notice View the rewards balance in this pool
      */
-    function getContractRewardsBalance() external override view returns (uint256) {
+    function getContractRewardsBalance()
+        external
+        view
+        override
+        returns (uint256)
+    {
         return _getContractRewardsBalance();
     }
 
     /**
      * @notice View the pending rewards balance for a user
      */
-    function getPendingRewards() external override view returns (uint256) {
+    function getPendingRewards() external view override returns (uint256) {
         return _getPendingRewards(stakers[msg.sender]);
     }
 
     /**
      * @notice Return the time, in seconds, remaining for a stake to be claimed or unstaked
      */
-    function getRemainingLockTime() external override view returns (uint256) {
+    function getRemainingLockTime() external view override returns (uint256) {
         // Return the time remaining for the stake to be claimed or unstaked
         Staker storage staker = stakers[msg.sender];
         if (block.timestamp > staker.unlockTimestamp) {
@@ -164,22 +169,23 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
     }
 
     ////////////////////////////////////
-        /* Internal Functions */
+    /* Internal Functions */
     ////////////////////////////////////
 
-    function _getPendingRewards(Staker storage staker) internal view returns (uint256) {
+    function _getPendingRewards(
+        Staker storage staker
+    ) internal view returns (uint256) {
         // Mark when the token was staked
-        return _calculateRewards(
-            block.timestamp - staker.lastUpdatedTimestamp,
-            staker.numStaked, 
-            poolWeight,
-            periodLength
-        ) + staker.pendingRewards;
+        return
+            _calculateRewards(
+                block.timestamp - staker.lastUpdatedTimestamp,
+                staker.numStaked,
+                poolWeight,
+                periodLength
+            ) + staker.pendingRewards;
     }
 
-    function _stake(
-		uint256 tokenId
-	) internal {
+    function _stake(uint256 tokenId) internal {
         // Transfer their NFT to this contract
         IERC721(stakingToken).safeTransferFrom(
             msg.sender,
@@ -193,9 +199,7 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
         emit Staked(tokenId, 1, 0, address(stakingToken));
     }
 
-    function _claim(
-		Staker storage staker
-	) internal {
+    function _claim(Staker storage staker) internal {
         // Require the time lock to have passed
         _onlyUnlocked(staker.unlockTimestamp);
 
@@ -210,16 +214,16 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
             revert NoRewards();
         }
 
-        rewardsToken.transfer(
-            msg.sender,
-            rewards
-        );
+        rewardsToken.transfer(msg.sender, rewards);
 
         emit Claimed(rewards, rewardsToken);
     }
 
-    function _unstake(uint256 tokenId, Staker storage staker) internal onlySNFTOwner(tokenId) {
-		_onlyUnlocked(staker.unlockTimestamp);
+    function _unstake(
+        uint256 tokenId,
+        Staker storage staker
+    ) internal onlySNFTOwner(tokenId) {
+        _onlyUnlocked(staker.unlockTimestamp);
 
         _burn(tokenId);
 
@@ -230,12 +234,7 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
             tokenId
         );
 
-        emit Unstaked(
-            tokenId,
-            1,
-            0,
-            address(stakingToken)
-        );
+        emit Unstaked(tokenId, 1, 0, address(stakingToken));
     }
 
     function _getContractRewardsBalance() internal view returns (uint256) {
@@ -243,7 +242,7 @@ contract StakingERC721 is ERC721NonTransferrable, StakingPool, IStakingERC721 {
     }
 
     function _onlyUnlocked(uint256 unlockTimestamp) internal view {
-		// User is not staked or has not passed the time lock
+        // User is not staked or has not passed the time lock
         if (unlockTimestamp == 0 || block.timestamp < unlockTimestamp) {
             revert TimeLockNotPassed();
         }
