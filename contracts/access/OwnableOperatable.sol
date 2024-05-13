bcbf5286cca1b40a11b499a6dbadb496d30ede73
Whytecrowe <dor7an@gmail.com>
2024-05-13 14:21:37 -0700

    create new OwnableOperatable contract to add operators to the Match contract and not use a single owner

---
+++ contracts/access/OwnableOperatable.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IOwnableOperatable } from "./IOwnableOperatable.sol";
+
+
+contract OwnableOperatable is Ownable, IOwnableOperatable {
+    mapping(address => bool) internal operators;
+
+    modifier onlyAuthorized() {
+        if (msg.sender != owner() || !operators[msg.sender])
+            revert NotAuthorized(msg.sender);
+        _;
+    }
+
+    function addOperator(address operator) external override onlyOwner {
+        if (operator == address(0)) revert ZeroAddress();
+
+        operators[operator] = true;
+        emit OperatorAdded(operator);
+    }
+
+    function removeOperator(address operator) external override onlyOwner {
+        operators[operator] = false;
+        emit OperatorRemoved(operator);
+    }
+
+    function isOperator(address operator) external view override returns (bool) {
+        return operators[operator];
+    }
+}
