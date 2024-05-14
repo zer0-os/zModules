f705395c1646e9bb262ba1ad0577780766e4fdb9
Whytecrowe <dor7an@gmail.com>
2024-05-14 14:57:38 -0700

    add new tests, remove outdated tests, fix existing

--- test/match.test.ts
+++ test/match.test.ts
@@ -2,17 +2,18 @@ import * as hre from "hardhat";
 import { ethers } from "ethers";
 import { expect } from "chai";
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
-import { Match, ERC20TestToken } from "../typechain";
+import { Match, ERC20TestToken, Match__factory } from "../typechain";
 import { getMatchDataHash } from "./helpers/match/hashing";
-import { getMatchStartedEvents } from "./helpers/match/events";
+import { getMatchEvents } from "./helpers/match/events";
 import {
   ARRAY_MISMATCH_ERR,
   INSUFFICIENT_FUNDS_ERR,
   INVALID_MATCH_ERR,
   MATCH_STARTED_ERR,
-  NO_PLAYERS_ERR,
-  ONLY_OWNER_ERR,
+  NO_PLAYERS_ERR, NOT_AUTHORIZED_ERR,
+  ONLY_OWNER_ERR, ZERO_ADDRESS_ERR,
 } from "./helpers/staking";
+import { getPayouts } from "./helpers/match/payouts";
 
 
 const getPlayerBalances = async (
@@ -43,9 +44,10 @@ describe("Match Contract",  () => {
   let wilderWallet : SignerWithAddress;
   let allPlayers : Array<SignerWithAddress>;
 
-  let ownerAddress : string;
   let mockERC20Address : string;
   let matchAddress : string;
+
+  let MatchFactory : Match__factory;
 
   before(async () => {
     [
@@ -61,7 +63,6 @@ describe("Match Contract",  () => {
       operator3,
       wilderWallet,
     ] = await hre.ethers.getSigners();
-    ownerAddress = await owner.getAddress();
 
     allPlayers = [
       player1,
@@ -72,14 +73,13 @@ describe("Match Contract",  () => {
       player6,
     ];
 
-    const MockERC20Factory = await hre.ethers.getContractFactory("ERC20TestToken");
+    const MockERC20Factory = await hre.ethers.getContractFactory("MockERC20");
     mockERC20 = (await MockERC20Factory.deploy("MockToken", "MTK")) as ERC20TestToken;
     mockERC20Address = await mockERC20.getAddress();
 
-    const MatchFactory = await hre.ethers.getContractFactory("Match");
+    MatchFactory = await hre.ethers.getContractFactory("Match");
     match = await MatchFactory.connect(owner).deploy(
       mockERC20Address,
-      ownerAddress,
       wilderWallet,
       [ operator3.address ]
     );
@@ -92,6 +92,16 @@ describe("Match Contract",  () => {
         await mockERC20.connect(player).increaseAllowance(matchAddress, ethers.parseEther("1000"));
       }, Promise.resolve()
     );
+  });
+
+  it("Should revert if wilderWallet is passed as 0x0 address", async () => {
+    await expect(
+      MatchFactory.connect(owner).deploy(
+        mockERC20Address,
+        ethers.ZeroAddress,
+        [ operator3.address ]
+      )
+    ).to.be.revertedWithCustomError(match, ZERO_ADDRESS_ERR);
   });
 
   describe("Aux Operations", () => {
@@ -123,28 +133,43 @@ describe("Match Contract",  () => {
     });
   });
 
-  describe.only("Matches", () => {
-    let balancesBefore : Array<bigint>;
-    let balancesAfter : Array<bigint>;
+  describe("Matches", () => {
+    let balancesBeforeStart : Array<bigint>;
+    let balancesAfterStart : Array<bigint>;
+    let balancesAfterEnd : Array<bigint>;
     let playerAddresses : Array<string>;
     let matchId : bigint;
-
-    const matchFee = ethers.parseEther("3.29");
-    const gameFee = ethers.parseEther("0.1");
+    let wilderWalletBalanceBefore : bigint;
     let matchDataHash : string;
 
+    const matchFee = ethers.parseEther("3.29");
+    const gameFeeBase = ethers.parseEther("0.1");
+    let payouts : Array<bigint>;
+    let gameFee : bigint;
+
     describe("#startMatch()", () => {
+      it("Should revert if called by a non-owner/non-operator", async () => {
+        await expect(
+          match.connect(player1).startMatch(
+            1,
+            [player1.address, player2.address],
+            ethers.parseEther("1")
+          )
+        ).to.be.revertedWithCustomError(match, NOT_AUTHORIZED_ERR)
+          .withArgs(player1.address);
+      });
+
       it("Should fail if any player is not funded", async () => {
         // Assuming addr1 and addr2 have insufficient balance
         const players = [player1.address, player2.address];
-        const matchFee = ethers.parseEther("10000000000000000000000000");
-        await expect(match.startMatch(7, players, matchFee))
+        const matchFeeNew = ethers.parseEther("10000000000000000000000000");
+        await expect(match.startMatch(7, players, matchFeeNew))
           .to.be.revertedWithCustomError(match, INSUFFICIENT_FUNDS_ERR);
       });
 
       it("Should not start a match with an empty players array", async () => {
-        const matchFee = ethers.parseUnits("1", "wei"); // Smallest possible entry fee
-        await expect(match.startMatch(9, [], matchFee))
+        const matchFeeNew = ethers.parseUnits("1", "wei"); // Smallest possible entry fee
+        await expect(match.startMatch(9, [], matchFeeNew))
           .to.be.revertedWithCustomError(match, NO_PLAYERS_ERR);
       });
 
@@ -160,7 +185,7 @@ describe("Match Contract",  () => {
           }, Promise.resolve([])
         );
 
-        balancesBefore = await getPlayerBalances(playerAddresses, match);
+        balancesBeforeStart = await getPlayerBalances(playerAddresses, match);
 
         matchDataHash = getMatchDataHash({
           matchId,
@@ -178,7 +203,7 @@ describe("Match Contract",  () => {
             matchFee: emittedmatchFee,
             fundsLocked: emittedFundsLocked,
           },
-        }] = await getMatchStartedEvents({ match });
+        }] = await getMatchEvents({ match });
 
         expect(emittedMatchDataHash).to.equal(matchDataHash);
         expect(emittedMatchId).to.equal(matchId);
@@ -188,12 +213,22 @@ describe("Match Contract",  () => {
         expect(emittedmatchFee).to.equal(matchFee);
         expect(emittedFundsLocked).to.equal(matchFee * BigInt(playerAddresses.length));
 
-        balancesAfter = await getPlayerBalances(playerAddresses, match);
+        balancesAfterStart = await getPlayerBalances(playerAddresses, match);
+
+        // get payouts for the tests below
+        ({
+          payouts,
+          gameFee,
+        } = getPayouts({
+          playerCount: BigInt(allPlayers.length),
+          matchFee,
+          gameFeeBase,
+        }));
       });
 
       it("Should exempt the entry fee from all the player balances", async () => {
-        balancesBefore.forEach((bal, index) => {
-          expect(bal - balancesAfter[index]).to.equal(matchFee);
+        balancesBeforeStart.forEach((bal, index) => {
+          expect(bal - balancesAfterStart[index]).to.equal(matchFee);
         });
       });
 
@@ -209,6 +244,19 @@ describe("Match Contract",  () => {
     });
 
     describe("#endMatch()", () => {
+      it("Should revert if called by a non-owner/non-operator", async () => {
+        await expect(
+          match.connect(player1).endMatch(
+            1,
+            [player1.address, player2.address],
+            [ethers.parseEther("1"), ethers.parseEther("1")],
+            ethers.parseEther("1"),
+            ethers.parseEther("0.1")
+          )
+        ).to.be.revertedWithCustomError(match, NOT_AUTHORIZED_ERR)
+          .withArgs(player1.address);
+      });
+
       it("Should fail if the match does not exist", async () => {
         const invalidMatchId = 999; // Use an ID for a match that doesn't exist
         await expect(
@@ -222,7 +270,7 @@ describe("Match Contract",  () => {
         ).to.be.revertedWithCustomError(match, INVALID_MATCH_ERR);
       });
 
-      it("Should fail if winners and winAmounts array lengths mismatch", async () => {
+      it("Should fail if players and payouts array lengths mismatch", async () => {
         await expect(
           match.endMatch(
             matchId,
@@ -234,31 +282,42 @@ describe("Match Contract",  () => {
         ).to.be.revertedWithCustomError(match, ARRAY_MISMATCH_ERR);
       });
 
+      it("Should revert if payout amounts and/or gameFee are calculated incorrectly", async () => {
+        const invalidPayouts = [1n, 1n, 1n, 1n, 1n, 1n];
+        await expect(
+          match.endMatch(
+            matchId,
+            playerAddresses,
+            invalidPayouts,
+            matchFee,
+            gameFee
+          )
+        ).to.be.revertedWithCustomError(match, INVALID_MATCH_ERR);
+
+        await expect(
+          match.endMatch(
+            matchId,
+            playerAddresses,
+            payouts,
+            matchFee,
+            gameFeeBase // using a gameFeeBase that is less than gameFee, so it fails
+          )
+        ).to.be.revertedWithCustomError(match, INVALID_MATCH_ERR);
+      });
+
       it("Should end the match and emit event with correct parameters", async () => {
-        const totalPayout = matchFee * BigInt(playerAddresses.length);
+        const totalPayoutRef = matchFee * BigInt(playerAddresses.length);
 
-        // TODO esc: figure out a proper function to calc these payouts without rounding problems !!!!
-        const payout1st = totalPayout / 3n;
-        const payout2nd = totalPayout / 4n;
-        const payout3rd = totalPayout / 5n;
-        const payoutRest = (totalPayout - (payout1st + payout2nd + payout3rd)) / (BigInt(playerAddresses.length) - 3n);
-        const payouts = [
-          payout1st,
-          payout2nd, // 113,998,499,999,999,999,993
-          payout3rd, // 19,740,000,000,000,000,000
-          payoutRest,
-          payoutRest,
-          payoutRest,
-        ];
-
-        const totalPayoutRef = payouts.reduce(
-          (sum, payout) : bigint => {
-            sum += payout;
-            return sum;
-          }, 0n
+        const totalPayout = payouts.reduce(
+          (acc, payout) => acc + payout,
+          0n
         );
 
-        expect(totalPayoutRef + gameFee).to.equal(totalPayout);
+        // make sure our helper calculated properly and there is no leftover,
+        // because if we get any leftover the contract will revert
+        expect(totalPayout + gameFee).to.equal(totalPayoutRef);
+
+        wilderWalletBalanceBefore = await match.balances(wilderWallet.address);
 
         await match.endMatch(
           matchId,
@@ -268,41 +327,71 @@ describe("Match Contract",  () => {
           gameFee
         );
 
-        // TODO esc: add event test !!
+        const [{
+          args: {
+            matchDataHash: emittedMatchDataHash,
+            matchId: emittedMatchId,
+            players: emittedPlayers,
+            payouts: emittedPayouts,
+            matchFee: emittedMatchFee,
+            gameFee: emittedGameFee,
+          },
+        }] = await getMatchEvents({
+          match,
+          eventName: "MatchEnded",
+        });
+
+        expect(emittedPlayers.hash).to.equal(
+          ethers.solidityPackedKeccak256(["address[]"], [playerAddresses])
+        );
+        expect(emittedMatchDataHash).to.equal(matchDataHash);
+        expect(emittedMatchId).to.equal(matchId);
+        expect(emittedPayouts).to.deep.equal(payouts);
+        expect(emittedMatchFee).to.equal(matchFee);
+        expect(emittedGameFee).to.equal(gameFee);
       });
-    });
-  });
 
-  describe("End Match", () => {
-    const matchId = 0;
-    it("Should fail if the match does not exist", async () => {
-      const invalidMatchId = 999; // Use an ID for a match that doesn't exist
-      await expect(match.endMatch(invalidMatchId, [player1.address], [ethers.parseEther("1")]))
-        .to.be.revertedWith("Match does not exist");
-    });
+      it("Should remove the locked amount from the #fundLocks mapping", async () => {
+        const lockedForMatch = await match.fundLocks(matchDataHash);
+        expect(lockedForMatch).to.equal(0n);
+      });
 
-    it("Should fail if winners and winAmounts array lengths mismatch", async () => {
-      await expect(match.endMatch(matchId, [player1.address], [ethers.parseEther("1"), ethers.parseEther("2")]))
-        .to.be.revertedWith("Array lengths mismatch");
-    });
+      it("Should disperse the payouts correctly to the player #balances", async () => {
+        balancesAfterEnd = await getPlayerBalances(playerAddresses, match);
 
-    it("Should correctly end the match and pay winners", async () => {
-      const winners = [player1.address, player2.address];
-      const winAmounts = [ethers.parseEther("1"), ethers.parseEther("2")];
+        playerAddresses.forEach(
+          (playerAddr, index) => {
+            const balBefore = balancesAfterStart[index];
+            const balAfter = balancesAfterEnd[index];
+            const payout = payouts[index];
 
-      // Balances before ending the match
-      const initialBalances = await Promise.all(winners.map(async winner => await match.balance(winner)));
+            expect(balAfter - balBefore).to.equal(payout);
+          }
+        );
+      });
 
-      // End the match
-      const tx = await match.endMatch(matchId, winners, winAmounts);
-      await tx.wait();
+      it("Should add #gameFee to the #wilderWallet balance", async () => {
+        const wilderWalletBalAfter = await match.balances(wilderWallet.address);
 
-      // Validate winners' balances increased by winAmounts
-      await Promise.all(winners.map(async (winner, index) => {
-        const finalBalance = await match.balance(winner);
-        const expectedBalance = initialBalances[index] + winAmounts[index];
-        expect(finalBalance).to.equal(expectedBalance);
-      }));
+        expect(wilderWalletBalAfter - wilderWalletBalanceBefore).to.equal(gameFee);
+      });
+
+      it("Players should be able to withdraw their winnings", async () => {
+        await allPlayers.reduce(
+          async (acc, player, idx) => {
+            await acc;
+            await match.connect(player).withdraw(payouts[idx], false);
+          }, Promise.resolve()
+        );
+
+        const balancesAfterWithdraw = await getPlayerBalances(playerAddresses, match);
+
+        balancesAfterWithdraw.forEach(
+          (bal, index) => {
+            expect(bal).to.equal(balancesAfterStart[index]);
+          }
+        );
+      });
     });
   });
 
@@ -310,35 +399,59 @@ describe("Match Contract",  () => {
     it("owner, operators assigned at deploy and operators assigned later should have appropriate rights", async () => {
       await match.connect(owner).addOperators([operator1.address, operator2.address]);
 
-      await match.connect(player1).deposit(ethers.parseEther("1"));
-      await match.connect(player2).deposit(ethers.parseEther("1"));
-
-      await [
+      const operators = [
         owner,
         operator1,
         operator2,
         operator3, // this was assigned at construction. they all should work equally
-      ].reduce(
-        async (acc, operator) => {
+      ];
+      const depositAmt = ethers.parseEther(operators.length.toString());
+
+      await match.connect(player1).deposit(depositAmt);
+      await match.connect(player2).deposit(depositAmt);
+      await match.connect(player3).deposit(depositAmt);
+      await match.connect(player4).deposit(depositAmt);
+
+      const matchFeeAC = ethers.parseEther("1");
+      const gameFeeBaseAC = ethers.parseEther("0.1");
+
+      const players = [
+        player1.address,
+        player2.address,
+        player3.address,
+        player4.address,
+      ];
+
+      await operators.reduce(
+        async (acc, operator, idx) => {
           await acc;
 
           await expect(
             match.connect(operator).startMatch(
-              1,
-              [player1.address, player2.address],
-              ethers.parseEther("1")
+              idx + 1,
+              players,
+              matchFeeAC
             )
           ).to.be.fulfilled;
 
-          // await expect(
-          await match.connect(operator).endMatch(
-            1,
-            [player1.address, player2.address],
-            [ethers.parseEther("0.45"), ethers.parseEther("0.45")],
-            ethers.parseEther("1"),
-            ethers.parseEther("0.1")
-          );
-          // ).to.be.fulfilled;
+          const {
+            payouts: payoutsAC,
+            gameFee: gameFeeAC,
+          } = getPayouts({
+            playerCount: BigInt(players.length),
+            matchFee: matchFeeAC,
+            gameFeeBase: gameFeeBaseAC,
+          });
+
+          await expect(
+            match.connect(operator).endMatch(
+              idx + 1,
+              players,
+              payoutsAC,
+              matchFeeAC,
+              gameFeeAC
+            )
+          ).to.be.fulfilled;
 
           await expect(
             match.connect(operator).setWilderWallet(operator1.address)
@@ -350,7 +463,7 @@ describe("Match Contract",  () => {
             ).to.be.revertedWith(ONLY_OWNER_ERR);
           } else {
             await expect(
-              match.connect(operator).transferOwnership(operator1.address)
+              match.connect(operator).transferOwnership(player1.address)
             ).to.be.fulfilled;
           }
         }, Promise.resolve()
@@ -358,7 +471,7 @@ describe("Match Contract",  () => {
 
       // set back
       await match.connect(operator2).setWilderWallet(wilderWallet.address);
-      await match.connect(operator1).transferOwnership(owner.address);
+      await match.connect(player1).transferOwnership(owner.address);
     });
   });
 });
