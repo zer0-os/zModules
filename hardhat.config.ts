2bdb151685a44042c0835ab6f8129465782f7c7b
Whytecrowe <dor7an@gmail.com>
2024-07-12 13:55:15 -0700

    add and comment out testnet values in hardhat.config for sepolia and meowtestnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -60,20 +60,47 @@ const config : HardhatUserConfig = {
     // //   gasPrice: 80000000000,
     // },
     // sepolia: {
-    //   // url: `${process.env.SEPOLIA_RPC_URL}`,
-    //   // timeout: 10000000,
-    //   // accounts: [ // Comment out for CI, uncomment this when using Sepolia
-    //   //   `${process.env.TESTNET_PRIVATE_KEY_A}`,
-    //   //   `${process.env.TESTNET_PRIVATE_KEY_B}`,
-    //   //   `${process.env.TESTNET_PRIVATE_KEY_C}`,
-    //   //   `${process.env.TESTNET_PRIVATE_KEY_D}`,
-    //   //   `${process.env.TESTNET_PRIVATE_KEY_E}`,
-    //   //   `${process.env.TESTNET_PRIVATE_KEY_F}`,
-    //   // ],
-    //   // // Must have to avoid instead failing as `invalid length for result data` error
-    //   // throwOnCallFailures: false, // not sure if this even works
+    //   url: `${process.env.SEPOLIA_RPC_URL}`,
+    //   timeout: 10000000,
+    //   accounts: [ // Comment out for CI, uncomment this when using Sepolia
+    //     `${process.env.TESTNET_PRIVATE_KEY_A}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_B}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_C}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_D}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_E}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_F}`,
+    //   ],
+    //   // Must have to avoid instead failing as `invalid length for result data` error
+    //   throwOnCallFailures: false, // not sure if this even works
+    // },
+    // meowtestnet: {
+    //   url: `${process.env.MEOWTESTNET_RPC_URL}`,
+    //   chainId: 883424730,
+    //   accounts: [ // Comment out for CI, uncomment this when using Sepolia
+    // `${process.env.DEPLOYER_PRIVATE_KEY}`,
+    // `${process.env.ZERO_VAULT_PRIVATE_KEY}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_A}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_B}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_C}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_D}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_E}`,
+    // `${process.env.TESTNET_PRIVATE_KEY_F}`,
+    //   ],
     // },
   },
+  // etherscan: {
+  //   apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+  //   customChains: [
+  //     {
+  //       network: "meowtestnet",
+  //       chainId: 883424730,
+  //       urls: {
+  //         apiURL: "https://meowchain-testnet-blockscout.eu-north-2.gateway.fm/api/",
+  //         browserURL: "https://meowchain-testnet-blockscout.eu-north-2.gateway.fm/",
+  //       },
+  //     },
+  //   ],
+  // },
   docgen: {
     pages: "files",
     templates: "docs/docgen-templates",
