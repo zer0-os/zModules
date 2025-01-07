779ff614fb1077524e3980e57cd0d3c904ab2ceb
Whytecrowe <dor7an@gmail.com>
2025-01-07 14:00:53 -0800

    create a file for upcoming deploy test

---
+++ test/deploy.integration.test.ts
@@ -0,0 +1,11 @@
+
+
+describe("zModules Deploy Integration Test", () => {
+  describe("Staking", () => {
+    it("Should deploy StakingERC20 with zDC and default config", () => {
+      // 1. getConfig()
+      // 2. run campaign function
+      // 3. check state values after deploy against default values in staking20Config
+    });
+  });
+});
