bfba41908f63a6a19b81181219cf67828574fdd4
MichaelKorchagin <benderrodriguez932@Gmail.com>
2025-07-14 15:01:00 -0700

    Auto-deploy for Rewards Vault finished.

--- src/deploy/campaign/rewards-vault-config.ts
+++ src/deploy/campaign/rewards-vault-config.ts
@@ -3,6 +3,7 @@ import { getRewardsVaultConfig } from "../missions/rewards-vault/rewards-vault.c
 import { getBaseZModulesConfig } from "./base-campaign-config";
 import { IZModulesConfig } from "./types";
 
+
 export const rewardsVaultSystemConfig = async (deployAdmin : SignerWithAddress) : Promise<IZModulesConfig> => {
   const baseConfig = await getBaseZModulesConfig({ deployAdmin });
   return {
--- src/deploy/missions/rewards-vault/rewards-vault.mission.ts
+++ src/deploy/missions/rewards-vault/rewards-vault.mission.ts
@@ -2,7 +2,7 @@ import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 import { BaseDeployMission, TDeployArgs } from "@zero-tech/zdc";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { IZModulesConfig, IZModulesContracts } from "../../campaign/types";
-import * as hre from "hardhat";
+
 
 export class ZModulesRewardsVaultDM extends BaseDeployMission<
 HardhatRuntimeEnvironment,
@@ -16,6 +16,8 @@ IZModulesContracts
 
   contractName = "ZeroRewardsVault";
   instanceName = "zeroRewardsVault";
+
+  operatorsToSet ?: Array<string> = [];
 
   async deployArgs () : Promise<TDeployArgs> {
     const {
@@ -36,34 +38,7 @@ IZModulesContracts
   async needsPostDeploy () : Promise<boolean> {
     const {
       config: {
-        rewardsVaultConfig,
-      },
-      zeroRewardsVault,
-    } = this.campaign;
-
-    if (!rewardsVaultConfig)
-      throw new Error("Rewards Vault config is not defined!");
-
-    const {
-      owner,
-    } = rewardsVaultConfig;
-
-    const signerOwner = await hre.ethers.getSigner(owner);
-
-    for (const operator of rewardsVaultConfig.operators) {
-      if (!(await zeroRewardsVault.connect(signerOwner).isOperator(operator))) {
-        return true;
-      } else {
-        return false; // If any operator is already set, no need for post-deploy
-      }
-    }
-
-    return false; // If no operators are defined, no post-deploy needed
-  }
-
-  async postDeploy () : Promise<void> {
-    const {
-      config: {
+        deployAdmin,
         rewardsVaultConfig,
       },
       zeroRewardsVault,
@@ -74,15 +49,43 @@ IZModulesContracts
 
     const {
       operators,
+    } = rewardsVaultConfig;
+
+    let needs = false;
+    if (!operators || operators.length !== 0) {
+      for (const operator of operators) {
+        if (!await zeroRewardsVault.connect(deployAdmin).isOperator(operator)) {
+          this.operatorsToSet?.push(operator);
+          needs = true;
+        }
+      }
+    }
+
+    return needs; // If no operators are defined, no post-deploy needed
+  }
+
+  async postDeploy () : Promise<void> {
+    const {
+      config: {
+        deployAdmin,
+        rewardsVaultConfig,
+      },
+      zeroRewardsVault,
+    } = this.campaign;
+
+    if (!rewardsVaultConfig)
+      throw new Error("Rewards Vault config is not defined!");
+
+    const {
       owner,
     } = rewardsVaultConfig;
 
-    const signerOwner = await hre.ethers.getSigner(owner);
-
-    if (!operators || operators.length === 0)
+    if (!this.operatorsToSet || this.operatorsToSet.length === 0)
       throw new Error("Operators are not defined!");
 
-    for (const operator of operators)
-      await zeroRewardsVault.connect(signerOwner).addOperator(operator);
+    for (const operator of this.operatorsToSet)
+      await zeroRewardsVault.connect(deployAdmin).addOperator(operator);
+
+    await zeroRewardsVault.connect(deployAdmin).transferOwnership(owner);
   }
 }
