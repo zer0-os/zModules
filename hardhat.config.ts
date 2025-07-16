658fe67877d552f6b07fe7de05cb9ea43c04d4ae
Whytecrowe <dor7an@gmail.com>
2025-07-16 14:56:32 -0700

    small updates

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -39,15 +39,6 @@ const config : HardhatUserConfig = {
           },
         },
       },
-      {
-        version: "0.8.26",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 20000,
-          },
-        },
-      },
     ],
   },
   paths: {
@@ -65,20 +56,64 @@ const config : HardhatUserConfig = {
   gasReporter: {
     enabled: false,
   },
-  networks: {},
-  // etherscan: {
-  //   apiKey: `${process.env.ETHERSCAN_API_KEY}`,
-  //   customChains: [
-  //     {
-  //       network: "meowtestnet",
-  //       chainId: 883424730,
-  //       urls: {
-  //         apiURL: "https://meowchain-testnet-blockscout.eu-north-2.gateway.fm/api/",
-  //         browserURL: "https://meowchain-testnet-blockscout.eu-north-2.gateway.fm/",
-  //       },
-  //     },
-  //   ],
-  // },
+  networks: {
+    zchain: {
+      chainId: 9369,
+      url: `${process.env.ZCHAIN_RPC_URL}`,
+      accounts: [
+        // `${process.env.DEPLOY_ADMIN_PRIVATE_KEY}`,
+      ],
+    },
+    zephyr: {
+      url: `${process.env.ZCHAIN_ZEPHYR_RPC_URL}`,
+      chainId: 1417429182,
+      accounts: [
+        // `${process.env.DEPLOY_ADMIN_ZCHAIN_ZEPHYR_PK}`,
+      ],
+      timeout: 10000000,
+      loggingEnabled: true,
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: [
+        // `${process.env.DEPLOY_ADMIN_FUJI_PK}`,
+      ],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      zchain: "placeholder", // any non-empty string will pass, not actually used by Snowtrace
+      zephyr: "placeholder",
+      fuji: "placeholder",
+    },
+    customChains: [
+      {
+        network: "zchain",
+        chainId: 9369,
+        urls: {
+          apiURL: "https://z-chain-blockscout.eu-north-2.gateway.fm/api/",
+          browserURL: "https://z-chain-blockscout.eu-north-2.gateway.fm/",
+        },
+      },
+      {
+        network: "zephyr",
+        chainId: 1417429182,
+        urls: {
+          apiURL: "https://zephyr-blockscout.eu-north-2.gateway.fm/api/",
+          browserURL: "https://zephyr-blockscout.eu-north-2.gateway.fm/",
+        },
+      },
+      {
+        network: "fuji",
+        chainId: 43113,
+        urls: {
+          apiURL: "https://api-testnet.snowtrace.io/api",
+          browserURL: "https://testnet.snowtrace.io"
+        }
+      },
+    ],
+  },
   docgen: {
     pages: "files",
     templates: "docs/docgen-templates",
--- src/deploy/scripts/staking-voting-20.ts
+++ src/deploy/scripts/staking-voting-20.ts
@@ -10,7 +10,6 @@ const deployStakingWithVotingToken = async () => {
   const [ deployAdmin ] = await hre.ethers.getSigners();
 
   const config = await getStaking20SystemConfig(
-    // !!! Admins here have to be manually set in this function to fine tune who admins what.
     deployAdmin,
   );
 
